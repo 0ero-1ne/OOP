321faacad7a1f859540461fd92833aad48acef2c
0ero-1ne <volikovd17@gmail.com>
2022-10-13 01:01:23 +0300

    [Lab 3] Added indexator, Pop and RemoveElement methods

--- LabThree/LabThree/LabArray.cs
+++ LabThree/LabThree/LabArray.cs
@@ -4,8 +4,20 @@ namespace LabThree
 {
     class Array
     {
+        public object this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
         object[] array;
-        private int length;
+        int length;
+
+        public Array()
+        {
+            length = 6;
+            array = new object[6] { 1, 2, 3, 4, 5, "Array" };
+        }
 
         public Array(int length, params object[] values)
         {
@@ -15,7 +27,6 @@ namespace LabThree
 
             this.length = length;
         }
-
 
         public void Push(object value)
         {
@@ -32,13 +43,40 @@ namespace LabThree
             return;
         }
 
+        public void Pop()
+        {
+            length -= 1;
+            object[] newArray = new object[length];
 
+            for (int i = 0; i < length; i++)
+                newArray[i] = array[i];
+
+            array = newArray;
+
+            return;
+        }
+
+        public void RemoveElement(int index)
+        {
+            if (index >= length || index < 0) throw new Exception("Invalid value for index");
+            length -= 1;
+
+            object[] newArray = new object[length];
+
+            for (int i = 0; i < index; i++)
+                newArray[i] = array[i];
+
+            for (int i = index; i < length; i++)
+                newArray[i] = array[i + 1];
+
+            array = newArray;
+        }
 
         public void PrintArray()
         {
             for (int i = 0; i < length; i++)
                 Console.WriteLine("Element {0} - {1}", i, array[i]);
-
+            Console.WriteLine("\n");
             return;
         }
     }
--- LabThree/LabThree/Program.cs
+++ LabThree/LabThree/Program.cs
@@ -1,12 +1,14 @@
-﻿namespace LabThree
+﻿using System;
+
+namespace LabThree
 {
     class Program
     {
         static void Main()
         {
-            Array arrayOne = new Array(5, 1, 2, 3, 4, 5);
-            arrayOne.Push(8);
-            arrayOne.PrintArray();
+            Array iArray = new Array(5, 1, 2, 3, 4, 5);
+            iArray.RemoveElement(4);
+            Console.WriteLine(iArray[0]);
         }
     }
 }
