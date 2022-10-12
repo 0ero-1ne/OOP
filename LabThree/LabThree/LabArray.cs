e2f1905a899e4b94eee8bcca58b1c1c17069d2ea
0ero-1ne <volikovd17@gmail.com>
2022-10-13 00:17:26 +0300

    Third lab starting

---
+++ LabThree/LabThree/LabArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LabThree
+{
+    class Array
+    {
+        object[] array;
+        private int length;
+
+        public Array(int length, params object[] values)
+        {
+            array = new object[length];
+            for (int i = 0; i < length; i++)
+                array[i] = values[i];
+
+            this.length = length;
+        }
+
+
+        public void Push(object value)
+        {
+            length += 1;
+            object[] newArray = new object[length];
+
+            newArray[length - 1] = value;
+
+            for (int i = 0; i < length - 1; i++)
+                newArray[i] = array[i];
+
+            array = newArray;
+
+            return;
+        }
+
+
+
+        public void PrintArray()
+        {
+            for (int i = 0; i < length; i++)
+                Console.WriteLine("Element {0} - {1}", i, array[i]);
+
+            return;
+        }
+    }
+}
---
+++ LabThree/LabThree/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LabThree
+{
+    class Program
+    {
+        static void Main()
+        {
+            Array arrayOne = new Array(5, 1, 2, 3, 4, 5);
+            arrayOne.Push(8);
+            arrayOne.PrintArray();
+        }
+    }
+}
