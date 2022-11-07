503f6c41cf13f14d8e4fdc554d8f3e70b52eaad7
0ero-1ne <volikovd17@gmail.com>
2022-11-07 15:35:34 +0300

    [lab 5] Partial class in different files

---
+++ LabFour/LabFour/RectanglePartial.cs
@@ -0,0 +1,16 @@
+﻿namespace LabFour
+{
+    partial class Rectangle : GeometricFigure
+    {
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() + 1;
+        }
+
+        public override void Resize(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+    }
+}
