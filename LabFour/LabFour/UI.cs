07961b7aaf1abc9fa886e6092c912969e0c30905
0ero-1ne <volikovd17@gmail.com>
2022-11-07 15:34:55 +0300

    [lab 5] UI class

---
+++ LabFour/LabFour/UI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+/*using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
+
+namespace LabFour
+{
+    class UI
+    {
+        public dynamic this[int index]
+        {
+            get => elements[index];
+            set => elements[index] = value;
+        }
+
+        List<dynamic> elements = new List<dynamic>();
+
+        public int Length => elements.Count;
+
+        public void AddElement(dynamic item)
+        {
+            elements.Add(item);
+        }
+
+        public void RemoveElement(int item)
+        {
+            elements.RemoveAt(item - 1);
+        }
+
+        public void PrintElements()
+        {
+            string result = "";
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                result += (i + ": ").ToString() + elements[i].ToString() + "\n";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
