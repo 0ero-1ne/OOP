2ae7bfd3c9d771c276fa93514f2c04ddd96084f3
0ero-1ne <volikovd17@gmail.com>
2022-11-16 16:15:11 +0300

    [lab 6] ValueOutOfRange exception

---
+++ LabFour/LabFour/ValueOutOfRange.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LabFour
+{
+    class ValueOutOfRange : Exception
+    {
+        public double Value { get; set; }
+        public ValueOutOfRange(string message, double value) : base(message)
+        {
+            Value = value;
+        }
+    }
+}
