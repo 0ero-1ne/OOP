c0e32ff99ade1446515ef675066d3700741cc22a
0ero-1ne <volikovd17@gmail.com>
2022-12-01 16:43:54 +0300

    [lab 8] Doone

---
+++ LabEight/LabEight/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LabEight
+{
+    public delegate void CompressHandler();
+    public delegate void MoveHandler(string destination);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("Dmitry", "Volikov", "Belarus", 18);
+
+            CompressHandler ch = new CompressHandler(Meth);
+
+            user1.Moved += (string destination) => Console.WriteLine("Moved to " + destination.ToString());
+            user1.Compressed += () => Console.WriteLine("Compressed");
+
+            Action<string, string, string> action = User.ReplaceString;
+            action += User.DeleteString;
+
+            Predicate<string> predicate = User.IsPalyndrom;
+
+            action.Invoke("Test string", "str", "ptr");
+            Console.WriteLine(predicate.Invoke("abbab"));
+
+            user1.MoveObject("Russia");
+            Console.WriteLine(user1.Country);
+        }
+
+        public static void Meth()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ LabEight/LabEight/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace LabEight
+{
+    class User
+    {
+        public event CompressHandler Compressed;
+        public event MoveHandler Moved;
+
+        public int CompressFactor { get; set; }
+
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Country { get; set; }
+        public int Age { get; set; }
+
+        public User
+        (
+            string firstName,
+            string secondName,
+            string country,
+            int age
+        )
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+            Country = country;
+            Age = age;
+        }
+
+        public static void ReplaceString(string str, string oldChar, string newChar) => Console.WriteLine(str.Replace(oldChar, newChar));
+
+        public static void DeleteString(string str, string delStr1, string delStr2)
+        {
+            if (str.Contains(delStr1))
+            {
+                Console.WriteLine(str.Replace(delStr1, ""));
+            }
+            else if (str.Contains(delStr2))
+            {
+                Console.WriteLine(str.Replace(delStr2, ""));
+            }
+            else
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        public static bool IsLenghtGreaterThanFive(string str) => str.Length > 5;
+
+        public static bool StringContainsA(string str) => str.Contains("A");
+
+        public static bool IsPalyndrom(string str)
+        {
+            for (int first = 0, last = str.Length - 1; first < last; ++first, --last)
+            {
+                if (str[first] != str[last])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void CompressObject()
+        {
+            if (Compressed != null)
+            {
+                CompressFactor = new Random().Next(100);
+                Compressed.Invoke();
+            }
+        }
+
+        public void MoveObject(string destination)
+        {
+            if (Moved != null)
+            {
+                Country = destination;
+                Moved.Invoke(destination);
+            }
+        }
+    }
+}
