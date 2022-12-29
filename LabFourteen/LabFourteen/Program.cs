caee0387339c418404ad8d2a03ed324b1d2c1941
0ero-1ne <volikovd17@gmail.com>
2022-12-30 00:30:18 +0300

    [lab 14] Тяжеловато

---
+++ LabFourteen/LabFourteen/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace LabFourteen
+{
+    class Program
+    {
+        static void Main()
+        {
+            //first task
+            var allProcesses = Process.GetProcesses();
+            foreach (var process in allProcesses)
+            {
+                try
+                {
+                    Console.WriteLine(
+                        $"Process ID: {process.Id}, Process name: {process.ProcessName}, Process prior: " +
+                        $"{process.BasePriority}, Start time: {process.StartTime}, " +
+                        $"Volume of virtual memory (bytes): {process.VirtualMemorySize64}, Process time: {process.TotalProcessorTime}.");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            //second task
+            var domain = AppDomain.CurrentDomain;
+            Console.WriteLine($"\nDomain name: {domain.FriendlyName}\nConfiguration details: {domain.SetupInformation}\n");
+            var currentBuilds = AppDomain.CurrentDomain.GetAssemblies();
+            Console.WriteLine("All build in application:");
+            foreach (var build in currentBuilds)
+            {
+                Console.WriteLine(build.GetName().Name);
+            }
+
+            try
+            {
+                AppDomain newDomain = AppDomain.CreateDomain("New domain");
+                newDomain.Load(@"D:\OOP\OOP\LabFourteen\LabFourteen\Task3.txt");
+                AppDomain.Unload(newDomain);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //task 3
+            var newThread = new Thread(ThreadInfo);
+            newThread.Start(Thread.CurrentThread);
+            Thread.Sleep(2000);
+            Console.Write("Enter n: ");
+            var userInput = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nSimple numbers from 1 to {userInput}:");
+            using (var newStream = new StreamWriter(@"D:\OOP\OOP\LabFourteen\LabFourteen\Task3.txt", false))
+            {
+                for (int i = 2; i < userInput; i++)
+                {
+                    if (IsPrime(i))
+                    {
+                        Console.Write($"{i} ");
+                        newStream.Write($"{i} ");
+                    }
+                }
+            }
+
+            //task4
+            Console.WriteLine("\nFirst even, than odd:");
+            Task4.FirstEvenThanOdd(userInput);
+            Console.WriteLine("\nEven-odd-even-odd:");
+            Task4.EvenOddChangingEveryTime(userInput);
+            Console.WriteLine();
+            //task5
+            int num = 5;
+            TimerCallback tm = new TimerCallback(Multiplier);
+            Timer timer = new Timer(tm, num, 200, 1000);
+            Thread.Sleep(5000);
+        }
+
+        private static bool IsPrime(int number)
+        {
+            for (var i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        private static void ThreadInfo(object thread)
+        {
+            var currentThread = thread as Thread;
+            Console.WriteLine($"\nThread name: {currentThread?.Name}");
+            Console.WriteLine($"Thread status: {currentThread.ThreadState}");
+            Console.WriteLine($"Thread prior: {currentThread.Priority}");
+            Console.WriteLine($"Thread ID: {currentThread.ManagedThreadId}");
+        }
+
+        private static void Multiplier(object obj)
+        {
+            Console.WriteLine("Iteration");
+        }
+    }
+}
---
+++ LabFourteen/LabFourteen/Task4.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace LabFourteen
+{
+    static class Task4
+    {
+        public static void FirstEvenThanOdd(int number)
+        {
+            object locker = new object();
+            Thread.Sleep(number * 1000 / 3);
+            Thread firstThread = new Thread(ShowEvenNumbers);
+            Thread secondThread = new Thread(ShowOddNumbers);
+
+            firstThread.Start(number);
+            secondThread.Start(number);
+
+
+            void ShowEvenNumbers(object userNumber)
+            {
+                if (userNumber == null) throw new ArgumentNullException(nameof(userNumber));
+                bool acquiredLock = false;
+                try
+                {
+                    Monitor.Enter(locker, ref acquiredLock);
+                    using (var firstThreadStream = new StreamWriter(@"D:\OOP\OOP\LabFourteen\LabFourteen\Task4.txt", true))
+                    {
+                        Console.WriteLine("\nThread 1");
+                        for (var i = 0; i < (int)userNumber; i++)
+                        {
+                            Thread.Sleep(100);
+                            if (i % 2 == 0)
+                            {
+                                Console.Write($"{i} ");
+                                firstThreadStream.Write($"{i} ");
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    if (acquiredLock)
+                    {
+                        Monitor.Exit(locker);
+                    }
+                }
+            }
+
+            void ShowOddNumbers(object anotherUserNumber)
+            {
+                bool acquiredLock = false;
+                try
+                {
+                    Console.WriteLine("\nThread2");
+                    Monitor.Enter(locker, ref acquiredLock);
+                    using (var secondThreadStream = new StreamWriter(@"D:\OOP\OOP\LabFourteen\LabFourteen\Task4.txt", true))
+                    {
+                        for (var i = 1; i < (int)anotherUserNumber; i++)
+                        {
+                            Thread.Sleep(100);
+                            if (i % 2 != 0)
+                            {
+                                Console.Write($"{i} ");
+                                secondThreadStream.Write($"{i} ");
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    if (acquiredLock)
+                    {
+                        Monitor.Exit(locker);
+                    }
+                }
+            }
+        }
+
+        public static void EvenOddChangingEveryTime(int number)
+        {
+            var mutex = new Mutex();
+            Thread firstThread = new Thread(ShowEvenNumbers);
+            Thread secondThread = new Thread(ShowOddNumbers);
+            Thread.Sleep(number * 1000 / 2);
+
+            firstThread.Start(number);
+            secondThread.Start(number);
+
+            void ShowEvenNumbers(object userNumber)
+            {
+                Console.WriteLine("\nThread 3");
+                for (var i = 0; i < (int)userNumber; i++)
+                {
+                    mutex.WaitOne();
+                    Thread.Sleep(10);
+                    if (i % 2 == 0)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                    mutex.ReleaseMutex();
+                }
+            }
+
+            void ShowOddNumbers(object anotherUserNumber)
+            {
+                Console.WriteLine("\nThread 4");
+                for (var i = 0; i < (int)anotherUserNumber; i++)
+                {
+                    mutex.WaitOne();
+                    Thread.Sleep(10);
+                    if (i % 2 != 0)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                    mutex.ReleaseMutex();
+                }
+
+            }
+        }
+    }
+}
